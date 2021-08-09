f02813f0d29309ffdfdc10c007f0e7e1230b1a68
mizuki-ito <yitengruixi397@gmail.com>
2021-08-09 15:29:55 +0900

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_021052) do
+ActiveRecord::Schema.define(version: 2021_08_07_004007) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_08_04_021052) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

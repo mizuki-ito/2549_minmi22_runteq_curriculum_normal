7c3c5bfc068a5f9d22bacff4996a43d370abdfb8
mizuki-ito <yitengruixi397@gmail.com>
2021-09-22 01:03:52 +0900

    add_ajax_to_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[update destroy]
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to board_path(comment.board), success: t('defaults.message.created', item: Comment.model_name.human)
-    else
-      redirect_to board_path(comment.board), danger: t('defaults.message.not_created', item: Comment.model_name.human)
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment.destroy!
   end
 
   private
+
+  def set_comment
+    @comment =current_user.comments.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
   end

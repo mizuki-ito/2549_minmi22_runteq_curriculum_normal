bc7aff8feef72bb1a32bcb23d0a680fdbdeaae6c
mizuki-ito <yitengruixi397@gmail.com>
2021-09-23 12:51:34 +0900

    pagination

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,7 @@
 class BoardsController < ApplicationController
   before_action :find_board, only: [:edit, :update, :destroy]
   def index
-    @boards = Board.all.includes(:user).order(created_at: :desc)
+    @boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def new
@@ -42,7 +42,7 @@ class BoardsController < ApplicationController
   end
 
   def bookmarks
-    @bookmark_boards = current_user.bookmark_boards.include(:user).order(created_at: :desc)
+    @bookmark_boards = current_user.bookmark_boards.include(:user).order(created_at: :desc).page(params[:page])
   end
 
   private
